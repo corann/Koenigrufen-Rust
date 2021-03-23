f760e0a2ac694ab55c5314b72157bfd17a1e6fbe
Andreas Traxler <andreas.traxler@gmail.com>
2021-03-23 21:09:21 +0100

    Added states for additional calls and kontras

--- src/main.rs
+++ src/main.rs
@@ -33,6 +33,7 @@ enum Player
 #[derive(Debug, Copy, Clone)]
 enum Players
 {
+    None,
     One(Player),
     Two(Player,Player),
     Three(Player,Player,Player),
@@ -44,21 +45,21 @@ enum CallStateA
 {
     Init,
     MeinSpiel,
-    Rufer(Option<CallKoenig>),
-    Piccolo( Players),
-    Zwiccolo( Players),
-    BesserRuferA(Option<CallKoenig>),
+    Rufer( Option< CallKoenig > ),
+    Piccolo( Players ),
+    Zwiccolo( Players ),
+    BesserRuferA( Option< CallKoenig > ),
     BesserRuferB( CallKoenig, Card ),
     Dreier,
     Farbensolo,
     SoloDreier,
     Bettler,
     BettlerOuvert,
-    PiccoloOuvert( Players),
-    ZwiccoloOuvert( Players),
+    PiccoloOuvert( Players ),
+    ZwiccoloOuvert( Players ),
     Trischaken,
     SechserDreier,
-    Solo(Option<CallKoenig>),
+    Solo( Option< CallKoenig > ),
 }
 
 #[derive(Debug, Copy, Clone, PartialOrd, PartialEq)]
@@ -77,9 +78,46 @@ struct CallStateB
 }
 
 #[derive(Debug, Copy, Clone)]
+enum CallStateAdditionsA
+{
+    KoenigUltimo,
+    VierKoenige,
+    I,
+    II,
+    III,
+    IV,
+    Trull,
+    Valat,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct CallStateAdditions
+{
+    addition: CallStateAdditionsA,
+    caller: Player,
+    active: Players,
+}
+
+#[derive(Debug, Copy, Clone)]
+enum CallStateKontrasA
+{
+    State( CallStateB ),
+    Addition( CallStateAdditions ),
+}
+
+#[derive(Debug, Copy, Clone)]
+struct CallStateKontras
+{
+    addition: CallStateKontrasA,
+    caller: Player,
+    active: Players,
+}
+
+#[derive(Debug, Clone)]
 struct CallStateC
 {
     state: CallStateB,
+    additions: Vec< CallStateKontras >,
     nextPlayer: Player,
 }
 
@@ -109,6 +147,11 @@ enum CallA
     Koenig(CallKoenig),
     Kleiner(Card),
     KoenigUltimo,
+    VierKoenige,
+    I,
+    II,
+    III,
+    IV,
     Trull,
     Valat,
 }
@@ -147,13 +190,22 @@ fn other_players( p: Player ) -> Players
 
 fn without_player( ps: Players, p: Player ) -> Players
 {
-    match p
+    let mut players = match ps
     {
-        Player::Geber => Players::Three( Player::Vorhand, Player::Player1, Player::Player2 ),
-        Player::Player1 => Players::Three( Player::Geber, Player::Vorhand, Player::Player2 ),
-        Player::Player2 => Players::Three( Player::Geber, Player::Player1, Player::Vorhand ),
-        Player::Vorhand => Players::Three( Player::Geber, Player::Player1, Player::Player2 ),
-    }
+        Players::None => vec![],
+        Players::One( p1 ) => vec![p1],
+        Players::Two( p1, p2 ) => vec![p1, p2],
+        Players::Three( p1, p2, p3 ) => vec![p1, p2, p3],
+        Players::Four( p1, p2, p3, p4 ) => vec![p1, p2, p3, p4],
+    };
+    players.retain(|&x|x==p);
+
+    if players.len() == 0 { return Players::None;}
+    if players.len() == 1 { return Players::One( players[0] );}
+    if players.len() == 2 { return Players::Two( players[0], players[1] );}
+    if players.len() == 3 { return Players::Three( players[0], players[1], players[2] );}
+    if players.len() == 4 { return Players::Four( players[0], players[1], players[2], players[3] );}
+    Players::None
 }
 
 fn next_player( p: Player ) -> Player
@@ -167,19 +219,19 @@ fn next_player( p: Player ) -> Player
     }
 }
 
-fn get_possible_calls( s: CallStateC, turn: Player ) -> Vec< CallB >
+fn get_possible_calls( s: &CallStateC, turn: Player ) -> Vec< CallB >
 {
     match s
     {
-        CallStateC{ state, nextPlayer }  => 
+        CallStateC{ state, additions, nextPlayer }  => 
         {
-            if nextPlayer != turn { return vec![]; }
+            if *nextPlayer != turn { return vec![]; }
 
             match state
             {
                 CallStateB{ state, caller, active } =>
                 {
-                    if !is_in_active(active, nextPlayer ) { return vec![]; }
+                    if !is_in_active( *active, *nextPlayer ) { return vec![]; }
 
                     match state
                     {
@@ -194,7 +246,7 @@ fn get_possible_calls( s: CallStateC, turn: Player ) -> Vec< CallB >
                             match (nextPlayer, caller, koenig)
                             {
                                 (np,Some(c), None) if np==c => Koenig::iter().map(|s| CallB::Call(CallA::Koenig(CallKoenig::Koenig(s)))).merge( vec![CallB::Call(CallA::Koenig(CallKoenig::VierterKoenig))]).collect(),
-                                (np,Some(c), Some(k)) if np!=c => vec![CallB::Kontra( CallA::Rufer, c), CallB::Kontra( CallA::Koenig(k), c)],
+                                (np,Some(c), Some(k)) if np!=c => vec![CallB::Kontra( CallA::Rufer, *c), CallB::Kontra( CallA::Koenig(*k), *c)],
                                 _ => vec![],
                             },
                         _ => vec![]
@@ -209,13 +261,14 @@ fn apply( s: CallStateC, c: CallB, p: Player ) -> CallStateC
 {
     match s
     {
-        CallStateC{ state, nextPlayer } => match state
+        CallStateC{ state, additions, nextPlayer } => match state
         {
             CallStateB{ state, caller, active } => match c
             {
-                CallB::Call( CallA::MeinSpiel ) => CallStateC{ state: CallStateB{ state: CallStateA::MeinSpiel, caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
-                CallB::Call( CallA::Weiter ) => CallStateC{ state: CallStateB{ state: state, caller: caller, active: without_player( active, p ) }, nextPlayer: next_player( p ) },
-                CallB::Call( CallA::Rufer ) => CallStateC{ state: CallStateB{ state: CallStateA::Rufer(None), caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
+                CallB::Call( CallA::MeinSpiel ) => CallStateC{ state: CallStateB{ state: CallStateA::MeinSpiel, caller: Some( p ), active: other_players( p ) }, additions: additions.clone(), nextPlayer: next_player( p ) },
+                CallB::Call( CallA::Weiter ) => CallStateC{ state: CallStateB{ state: state.clone(), caller: caller.clone(), active: without_player( active.clone(), p ) }, additions: additions.clone(), nextPlayer: next_player( p ) },
+                CallB::Call( CallA::Rufer ) => CallStateC{ state: CallStateB{ state: CallStateA::Rufer(None), caller: Some( p ), active: other_players( p ) }, additions: additions.clone(), nextPlayer: next_player( p ) },
+                //CallB::Kontra()
                 // CallB::Call( CallA::Piccolo ) => CallStateC{ state: CallStateB{ state: CallStateA::MeinSpiel, caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
                 // CallB::Call( CallA::PiccoloMit ) => CallStateC{ state: CallStateB{ state: CallStateA::MeinSpiel, caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
                 // CallB::Call( CallA::Zwiccolo ) => CallStateC{ state: CallStateB{ state: CallStateA::MeinSpiel, caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
@@ -238,7 +291,7 @@ fn apply( s: CallStateC, c: CallB, p: Player ) -> CallStateC
                 // CallB::Call( CallA::KoenigUltimo ) => CallStateC{ state: CallStateB{ state: CallStateA::KoenigUltimo, caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
                 // CallB::Call( CallA::Trull ) => CallStateC{ state: CallStateB{ state: CallStateA::KoenigUltimo, caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
                 // CallB::Call( CallA::Valat ) => CallStateC{ state: CallStateB{ state: CallStateA::KoenigUltimo, caller: Some( p ), active: other_players( p ) }, nextPlayer: next_player( p ) },
-                _ => panic!(""),
+                _ => panic!("Action not yet implemented"),
             }
         }
     }
@@ -252,9 +305,9 @@ fn get_full_deck() -> Vec::<Card>{
         Card::Karo( RedSuit::Bub ),Card::Karo( RedSuit::Reiter ),Card::Karo( RedSuit::Dame ),Card::Karo( RedSuit::Koenig ),
         Card::Herz( RedSuit::Vier ),Card::Herz( RedSuit::Drei ),Card::Herz( RedSuit::Zwei ),Card::Herz( RedSuit::As ),
         Card::Herz( RedSuit::Bub ),Card::Herz( RedSuit::Reiter ),Card::Herz( RedSuit::Dame ),Card::Herz( RedSuit::Koenig ),
-        Card::Pik( BlackSuit:: Sieben ),Card::Pik( BlackSuit::Acht ),Card::Pik( BlackSuit::Neun ),Card::Pik( BlackSuit::Zehn ),
+        Card::Pik( BlackSuit::Sieben ),Card::Pik( BlackSuit::Acht ),Card::Pik( BlackSuit::Neun ),Card::Pik( BlackSuit::Zehn ),
         Card::Pik( BlackSuit::Bub ),Card::Pik( BlackSuit::Reiter ),Card::Pik( BlackSuit::Dame ),Card::Pik( BlackSuit::Koenig ),
-        Card::Treff( BlackSuit::  Sieben ),Card::Treff( BlackSuit::Acht ),Card::Treff( BlackSuit::Neun ),Card::Treff( BlackSuit::Zehn ),
+        Card::Treff( BlackSuit::Sieben ),Card::Treff( BlackSuit::Acht ),Card::Treff( BlackSuit::Neun ),Card::Treff( BlackSuit::Zehn ),
         Card::Treff( BlackSuit::Bub ),Card::Treff( BlackSuit::Reiter ),Card::Treff( BlackSuit::Dame ),Card::Treff( BlackSuit::Koenig ), ]
 }
 
@@ -268,8 +321,10 @@ fn main()
     //let state = CallStateB::State(CallStateA::Init,Player::Vorhand);
     let sa = CallStateA::Rufer( Some( CallKoenig::Koenig( Koenig::Karo ) ) );
     let sb = CallStateB{ state: sa, caller: Some( Player::Vorhand ), active: Players::Three( Player::Geber, Player::Player1, Player::Player2 ) };
-    let state = CallStateC{ state: sb, nextPlayer: Player::Player1 };
+    let state = CallStateC{ state: sb, additions: vec![], nextPlayer: Player::Player1 };
     println!("{:?}", state);
-    let pc = get_possible_calls(state, Player::Player1);
+    let pc = get_possible_calls(&state, Player::Player1);
     println!("{:?}", pc);
+    let newstate = apply(state, pc[0], Player::Player1);
+    println!("{:?}", newstate);
 }
